73cafe09d4bb44838f9c3a38a53bea4c3d5653a0
Oukanina <guo.ke.ke.a@gmail.com>
2024-03-26 02:33:48 +0800

    🦐

--- src/index.js
+++ src/index.js
@@ -15,3 +15,44 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+function preloadShrimp() {
+  const shrimpImage = new Image();
+  shrimpImage.src = '/shrimp.png';
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randomPosition(n) {
+  return (n / 2) - getRandomInt(n);
+}
+
+function shrimpAnimationEnd(target) {
+  target.addEventListener("animationend", (event) => {
+    target.remove();
+  });
+}
+
+function flyShrimp(event) {
+  const shrimpImage = new Image();
+  shrimpImage.src = '/shrimp.png';
+  shrimpImage.classList.add('🦐');
+  shrimpImage.style.top = `${event.offsetY + randomPosition(160)}px`;
+  shrimpImage.style.left = `${event.offsetX - 5 + randomPosition(160)}px`;
+  shrimpAnimationEnd(shrimpImage);
+  document.body.appendChild(shrimpImage);
+}
+
+function clickAnimation(event) {
+  flyShrimp(event);
+  flyShrimp(event);
+  flyShrimp(event);
+  flyShrimp(event);
+  flyShrimp(event);
+  flyShrimp(event);
+}
+
+preloadShrimp();
+document.body.addEventListener('click', clickAnimation, false)
